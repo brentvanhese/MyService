3a687ba25e96aecf86e1a727fdd6ddafb81a7ef2
Brent Van Hese <vanhesebrent@gmail.com>
2026-06-09 23:40:52 +0200

    fix: added history routes to disable back swipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
 import BottomNav from "./components/BottomNav/BottomNav";
@@ -7,6 +8,23 @@ import Settings from "./pages/Settings";
 
 function AppContent() {
 	const location = useLocation();
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: trying to fix
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		// Prevent back swipe by pushing a new history state
+		window.history.pushState(null, "", window.location.href);
+	}, [location.pathname]);
+
+	useEffect(() => {
+		// Handle back button/swipe - prevent navigation
+		const handlePopState = () => {
+			window.history.pushState(null, "", window.location.href);
+		};
+
+		window.addEventListener("popstate", handlePopState);
+		return () => window.removeEventListener("popstate", handlePopState);
+	}, []);
 
 	return (
 		<div className="app">
